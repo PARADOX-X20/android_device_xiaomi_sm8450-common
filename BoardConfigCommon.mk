b2417fab7664f5c6aa418dc42f2799e99883bfd2
PARADOX_X20 <sarkerpronoy48@gmail.com>
2025-09-19 04:26:09 +0600

    added test dolby support

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -213,13 +213,13 @@ $(foreach nfc_sku, $(call to-upper, $(TARGET_NFC_SUPPORTED_SKUS)), \
 endif
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(COMMON_PATH)/vintf/device_framework_matrix.xml \
     $(COMMON_PATH)/vintf/dolby_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/genesis/config/device_framework_matrix.xml
 
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/vintf/framework_manifest.xml
+
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
